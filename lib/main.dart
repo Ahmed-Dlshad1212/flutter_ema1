4ae3e7bb40eb362e50952f92cc8547ee117755af
Ahmed-Dlshad1212 <149675381+Ahmed-Dlshad1212@users.noreply.github.com>
2024-01-15 23:37:20 +0300

    stuff

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:ems/views/onboarding_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.toString());
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context)
+              .textTheme, // If this is not set, then ThemeData.light().textTheme is used.
+        ),
+      ),
+      title: 'Flutter Demo',
+      home: OnBoardingScreen(),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'my_widgets.dart';
+
+Widget CustomAppBar() {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 15),
+    child: Row(
+      children: [
+        Container(
+          width: 116,
+          height: 17,
+          child: myText(
+              text: 'EMS',
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16)),
+        ),
+        Spacer(),
+        Container(
+          width: 24,
+          height: 22,
+          child: InkWell(
+            onTap: () {},
+            child: Image.asset('assets/Frame.png'),
+          ),
+        ),
+        SizedBox(
+          width: Get.width * 0.04,
+        ),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            width: 22,
+            height: 20,
+            child: Image.asset(
+              'assets/menu.png',
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
